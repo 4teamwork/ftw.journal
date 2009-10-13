e8454006f23d3e2b09244dfc44f14da5a1c66274
Jonas Baumann <j.baumann@4teamwork.ch>
2009-10-13 07:58:49 +0000

    created version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = open('ftw/journal/version.txt').read().strip()
 
 setup(name='ftw.journal',
       version=version,
