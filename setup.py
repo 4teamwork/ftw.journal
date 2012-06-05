1ca5374e8bdee6c7863e8cc69fd58e5f29a5d44e
Jonas Baumann <jone@jone.ch>
2012-06-05 12:23:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ import os
 
 version = '1.2.5.dev0'
 maintainer = 'Jonas Baumann'
-tests_require = ['plone.app.testing',
-                 'plone.mocktestcase', ]
+
+tests_require = [
+    'plone.app.testing',
+    'plone.mocktestcase',
+    ]
+
 setup(name='ftw.journal',
       version=version,
       maintainer=maintainer,
       description="Journaling infrastructure for plone",
+
       long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -20,18 +26,22 @@ setup(name='ftw.journal',
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='plone workflow journal',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.journal',
+
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         ],
+
       tests_require=tests_require,
       extras_require = dict(tests=tests_require),
 
