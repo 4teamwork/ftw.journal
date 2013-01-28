95bc80fdcc1c76380f7949e32f4e32438f5e4683
Jonas Baumann <jone@jone.ch>
2013-01-28 11:12:05 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@ setup(name='ftw.journal',
 
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='plone workflow journal',
