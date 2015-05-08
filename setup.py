82403277de0552c1b9d045baa10dfdb28a7439aa
Jonas Baumann <jone@jone.ch>
2015-05-08 12:09:47 +0200

    4teamwork is an AG.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name='ftw.journal',
         ],
 
       keywords='plone workflow journal',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.journal',
 
